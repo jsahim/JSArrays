1e16422fe6c5e3b1def668441d8c1a4dc8a0c256
Jeff Sahim <jeffreysahim@gmail.com>
2022-09-21 10:17:14 -0400

    Add 3 Methods to Arrays

--- arrays.js
+++ arrays.js
@@ -1,3 +1,20 @@
+//The POP method removes the last element in the array//
 var ages = [27, 49, 10, 87];
+
+ages.pop();
+
+console.log(ages);
+
+//The SHIFT method removes the first element in the array//
 var names = ["John", "Peter", "Michael", "David"];
+
+names.shift();
+
+console.log(names);
+
+//The PUSH method adds one or more elements to the existing array if an object is added to the method syntax //
 var paidTuition = [true, false, false, true];
+
+paidTuition.push(false);
+
+console.log(paidTuition);
